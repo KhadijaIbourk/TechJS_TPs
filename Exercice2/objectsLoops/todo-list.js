e750467af0625f288a18becd5b78dcffcb52eee6
Khadija <khadija.ibourk.2003@gmail.com>
2025-09-29 21:41:43 +0100

    Ajouter l'exercice1 et l'exercice2

---
+++ Exercice2/objectsLoops/todo-list.js
@@ -0,0 +1,62 @@
+const todoList = [
+  {
+    name: 'review course',
+    dueDate: '2025-09-29'
+  }
+];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  // Loop over every toDo object and append it to "todoListHTML"
+  todoList.forEach((value, index) => {
+    const { name, dueDate } = value;
+
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  });
+
+  // Show the objects inside the class "js-todo-list"
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+  // Loop over every delete button and add an eventListener
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1); 
+        renderTodoList(); 
+      });
+    });
+}
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  // Add these values to the variable "todoList"
+  if (name && dueDate) {
+    todoList.push({
+      name,
+      dueDate
+    });
+  }
+
+  inputElement.value = '';
+  dateInputElement.value = '';
+
+  renderTodoList();
+}
